1a61a9d99938b9339a2aef58f40abdc915eca85d
Yukie765 <yliya-kate@mail.ru>
2024-10-24 22:14:33 +0500

    Update Module_10_2.py

--- Module_10_2.py
+++ Module_10_2.py
@@ -7,16 +7,16 @@ class Knight(Thread):
         super().__init__()
         self.name = name
         self.power = power
+        self.time_tick = 0
+        self.enemy_count = 100
 
-    time_tick = 0
-    enemy_count = 100
     def run(self):
         print(f"{self.name}, на нас напали!")
         while self.enemy_count!=0:
             sleep(1)
             self.time_tick += 1
             self.enemy_count = self.enemy_count - self.power
-            print(F'{self.name} сражается {self.time_tick} день(дня), осталось {self.enemy_count} воинов.')
+            print(f'{self.name} сражается {self.time_tick} день(дня), осталось {self.enemy_count} воинов.')
         print(f'{self.name}, одержал победу спустя {self.time_tick} дней(дня)!')
 
 first_knight = Knight('Sir Lancelot', 10)
